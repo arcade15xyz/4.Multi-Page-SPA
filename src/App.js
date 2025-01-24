6dcb87f714b7aea95331c6cdb55b57f7f2111fe9
arcade15xyz <90238231+arcade15xyz@users.noreply.github.com>
2025-01-24 11:44:59 +0530

    now using another aproach for routing using createRoutesFromElements used in old versions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, createRoutesFromElements} from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/products";
 
-const router = createBrowserRouter([
-  { path: '/', element: <HomePage /> },
-  { path: '/products', element: <ProductsPage />}
-])
+const routeDefinitions = createRoutesFromElements(
+  <Route>
+    <Route path='/'  element={<HomePage />} />
+    <Route path='/products'  element={<ProductsPage />} />
+  </Route>
+);
+
+const router = createBrowserRouter(routeDefinitions);
+// const router = createBrowserRouter([
+//   { path: '/', element: <HomePage /> },
+//   { path: '/products', element: <ProductsPage />}
+// ])
 
 function App() {
   return <RouterProvider router={router} />;
